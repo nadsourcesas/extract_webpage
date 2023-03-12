57d1ca6952d7d5afcab1bbfac81c67e306f5f0d5
misterbahaehmimdi <127527380+misterbahaehmimdi@users.noreply.github.com>
2023-03-12 15:45:59 +0100

    new

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return requests.get('https://cima4u1.autos/').text
+    return requests.get(requests.get('http://testi123.pythonanywhere.com/url/url').text).text
