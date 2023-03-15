9974a1ceb36b7c702aafb43402696939cebbc4a1
misterbahaehmimdi <127527380+misterbahaehmimdi@users.noreply.github.com>
2023-03-15 18:11:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def check():
     data = pd.read_excel("datacheck.xlsx")
     al=list(data.iterrows())
     lal=len(al)
-    for index2,d1 in al[:int(lal/2)]:
+    for index2,d1 in al[int(lal/2):]:
      try:
       nm=d1['name']
       print("-+-+-+",nm,"-+-+-+-+",d1['name']) 
