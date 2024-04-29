e23cc02fd11b8dc8916065fd423eb69eceea3b65
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2024-04-29 04:39:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def get_html(url):
     
     for prefix in prefixes:
         try:
-            response = requests.get(prefix + url,timeout=10)
+            response = requests.get(prefix + url)
             if response.status_code == 200:
                 return response.text
         except Exception as e:#requests.RequestException
