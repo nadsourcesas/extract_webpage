611311269afeb9accb3cbc4db342642a51c08d80
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2024-04-29 11:10:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,18 +13,28 @@ def hello_world():
     return "hello in bahae api"
 def get_html_text(url):
    try: 
-    prefixes = ['http://', 'https://', 'http://www.', 'https://www.']
-    
+    prefixes = ['https://', 'http://', 'https://www.', 'http://www.']
+    forcedprefix=False
     for prefix in prefixes:
+        
+            forcedprefix=prefix
+            prefixes.remove(prefix)
+            prefixes=[forcedprefix]+prefixes
+    for prefix in prefixes:
+        
         try:
-            response = requests.get(prefix + url[1:-1])
+            if prefix in url :
+                response = requests.get( url[1:-1]) 
+            else:    
+               response = requests.get(prefix + url[1:-1]) 
+           
             if response.status_code == 200:
-                return  jsonify({'status': 'success','prefix':prefix, 'data': BeautifulSoup(response.text).get_text()})
+                return  jsonify({'status': 'success',lasturl:str(response.history[-1].url),'prefix':prefix, 'data': BeautifulSoup(response.text).get_text()})
 
         except Exception as e:#requests.RequestException
             print(f"Error occurred while trying {prefix + url}: {e}")
     
-    return jsonify({'status': 'failed', 'data': '','prefix':''})
+    return jsonify({'status': 'failed',lasturl:'', 'data': '','prefix':''})
    except Exception as problem:
        return str(problem)
 def get_html(url):
@@ -39,9 +49,10 @@ def get_html(url):
             print(f"Error occurred while trying {prefix + url}: {e}")
     
     return "nothing worked"
+
 @app.route('/taskhtml/<string:name>')
 def taskhtml(name):
  return get_html(name)
-@app.route('/task/<string:name>')
+@app.route('/task/<string:name>', strict_slashes=False)
 def task(name):
  return get_html_text(name)
