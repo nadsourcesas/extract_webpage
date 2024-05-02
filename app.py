0e42fa1298487bcbc5411b7628cdfe40a1da93c5
nadsourcesas <168539493+nadsourcesas@users.noreply.github.com>
2024-05-02 08:11:08 +0200

    Update app.py
    app (4)

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def get_html_text(url):
             response = requests.get(testedurl, allow_redirects=True)
            
             if response.status_code == 200:
-                soup=BeautifulSoup(response.text)
+                soup=BeautifulSoup(response.text, features='html.parser')
                 fj={'status': 'success','h1':soup.find('h1').get_text().strip(),'titles':tbl(soup),'topic':extract_title(soup),'metas':extract_meta_tags(soup),'final':str(response.url), 'data': soup.get_text()}#,'tst':str(tst),'testedurl':testedurl,'lasturl':str(list(map(lambda a:a.url,response.history))),
                 
            #     fj.update(scrape_headings_from_html(soup))
